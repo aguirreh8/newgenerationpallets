522b4dacd8e964a805a7c7b3abc7ce6d815d51ea
unknown <aguirreh8@gmail.com>
2018-05-16 23:11:50 -0400

    Add image to Jumbotron, added lead text in Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () =>(
 	<Router>
 		<Wrapper>
 			<Header /> 
+			<Route path="/" component={Home} />
 		</Wrapper>
 	</Router>
 );
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => (
+	<div>
+		<div className="Jumbotron">
+			<div className="container">
+				<h1>New Generation Pallets Inc.</h1>
+			</div>
+		</div>
+		<div className="container">
+			<p className="text-center home-text">We buy and sell pallets, serving New York and New Jersey.</p>
+		</div>
+	</div>
+)
+
+export default Home;
